b213774e437acdbfb59aa4bfff8b15b4f7fc637f
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-05-14 13:11:54 -0400

    GET routes built

--- app.js
+++ app.js
@@ -12,12 +12,20 @@ const todoList = [
     id: 1,
     todo: 'Implement a REST API',
   },
+  {
+    id: 2,
+    todo: 'Hello World!',
+  },
+  {
+    id: 3,
+    todo: 'Welcome to the wonderful world of Node.',
+  },
 ];
 
 // GET /api/todos
-
+app.get("/api/todos", (req, res) => res.send(todoList));
 // GET /api/todos/:id
-
+app.get("/api/todos/:id", (req, res) => res.send(todoList.filter(todo => todo.id == req.params.id)));
 // POST /api/todos
 
 // PUT /api/todos/:id
